5c1242e83cd339e9f3a9902086a36d51c5b2dde8
bonasaurus1 <Vilter@Downstairs-iMac.home>
2009-03-03 16:02:06 -0500

    tweaked routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'posts'
-  map.resources :users
-  map.resource :session
   map.resources :posts, :has_many => :comments
   # restful authentication
-  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
+  map.resources :users
+  map.resource :session
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
